dd1292e95d4ad466e18c1ed5ecd938f67f575c7f
alexferl <me@alexferl.com>
2022-11-21 15:30:58 -0500

    http: use RequestLoggerWithConfig instead of ziflex

--- http/go.mod
+++ http/go.mod
@@ -4,23 +4,22 @@ go 1.19
 
 require (
 	github.com/labstack/echo/v4 v4.9.1
-	github.com/labstack/gommon v0.4.0
+	github.com/rs/zerolog v1.28.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.13.0
-	github.com/ziflex/lecho/v3 v3.1.0
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
-	github.com/rs/zerolog v1.28.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
--- http/middleware/middleware.go
+++ http/middleware/middleware.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
-	"github.com/ziflex/lecho/v3"
 
 	"github.com/alexferl/golib/http/config"
 )
@@ -16,25 +18,58 @@ func Register(e *echo.Echo, mw ...echo.MiddlewareFunc) {
 		middleware.Recover(),
 		middleware.RequestID(),
 	}
-	mws = append(mw, mw...)
-	e.Use(mws...)
 
 	if viper.GetBool(config.HTTPCORSEnabled) {
-		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		cors := middleware.CORSWithConfig(middleware.CORSConfig{
 			AllowOrigins:     viper.GetStringSlice(config.HTTPCORSAllowOrigins),
 			AllowMethods:     viper.GetStringSlice(config.HTTPCORSAllowHeaders),
 			AllowHeaders:     viper.GetStringSlice(config.HTTPCORSAllowHeaders),
 			AllowCredentials: viper.GetBool(config.HTTPCORSAllowCredentials),
 			ExposeHeaders:    viper.GetStringSlice(config.HTTPCORSExposeHeaders),
 			MaxAge:           viper.GetInt(config.HTTPCORSMaxAge),
-		}))
+		})
+		mws = append(mws, cors)
 	}
 
 	if viper.GetBool(config.HTTPLogRequests) {
-		logger := lecho.From(log.Logger)
-		e.Logger = logger
-		e.Use(lecho.Middleware(lecho.Config{
-			Logger: logger,
-		}))
+		logger := middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+			LogRequestID:     true,
+			LogRemoteIP:      true,
+			LogHost:          true,
+			LogMethod:        true,
+			LogURI:           true,
+			LogUserAgent:     true,
+			LogStatus:        true,
+			LogError:         true,
+			LogLatency:       true,
+			LogContentLength: true,
+			LogResponseSize:  true,
+			LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+				i, _ := strconv.Atoi(v.ContentLength)
+				log.Logger.Info().
+					Str("time", time.Now().Format(time.RFC3339Nano)).
+					Str("id", v.RequestID).
+					Str("remote_id", v.RemoteIP).
+					Str("host", v.Host).
+					Str("method", v.Method).
+					Str("uri", v.URI).
+					Str("user_agent", v.UserAgent).
+					Int("status", v.Status).
+					Err(v.Error).
+					Int64("latency", v.Latency.Nanoseconds()).
+					Str("latency_human", v.Latency.String()).
+					Int64("bytes_in", int64(i)).
+					Int64("bytes_out", v.ResponseSize).
+					Send()
+
+				return nil
+			},
+		})
+
+		mws = append(mws, logger)
 	}
+
+	mws = append(mws, mw...)
+
+	e.Use(mws...)
 }
